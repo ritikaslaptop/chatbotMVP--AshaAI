adc8dce578ae544d06426b20d95551269879f459
Ritika <ritikaslaptop@gmail.com>
2025-04-22 23:08:44 +0530

    change db to SQLAlchemy compatibility

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from sqlalchemy.sql import func ##
 
 
 class Interaction(db.Model):
-    id = db.Column(db.String(36), primary_key=True)
-    session_id = db.Column(db.String(36), nullable=False, index=True)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    session_id = db.Column(db.String(80), nullable=False)
     user_message = db.Column(db.Text, nullable=False)
     bot_response = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)

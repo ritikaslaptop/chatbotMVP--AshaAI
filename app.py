cf5881dfef0f843adaf07a3a333c4e6a95f235b9
Ritika <ritikaslaptop@gmail.com>
2025-04-25 20:43:01 +0530

    railway connection handling

--- app.py
+++ app.py
@@ -13,7 +13,15 @@ from models import Interaction, BiasDetection, MetricsTracker
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-database_url = os.environ.get("DATABASE_URL", "sqlite:///temp.db")  # Fallback for local dev
+
+#railway connection handling
+database_url = os.environ.get("DATABASE_URL")
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    logger.info(f"Using database URL: {database_url}")
+else:
+    database_url = os.environ.get("DATABASE_URL", "sqlite:///temp.db")  # Fallback for local dev
+    logger.info(f"Using database URL (default or fallback): {database_url}")
 
 #init flask
 app = Flask(__name__)
